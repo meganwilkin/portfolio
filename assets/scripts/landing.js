4861c3c305802201be6eb0ad1bbc1a5407e33cda
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-02-18 15:38:52 +0000

    Fixed some responsive design issues

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -5,13 +5,24 @@ var scrollHandler = function(){
         $('.strap-line-upper').addClass('appear');
         $('.strap-line-lower').addClass('appear');
         $('.info-image').addClass('appear');
-        $(document).off('scroll', scollHandler);        
+        $(document).off('scroll', this);        
     }
 };
 
+var scrollToStrapline = function() {
+    $('html, body').animate({
+        scrollTop: $(".strap-line-upper").offset().top - 70
+    }, 2000);
+};
 
 $(document).ready(function(){
     $(this).on('scroll',scrollHandler);
-//    $('.strap-line-upper').hide();
-//    $('.strap-line-lower').hide();
+    
+    if ($('.strap-line').visible()) {
+        scrollHandler();
+    }
+    
+    $('.read-more-icon').click(function() {
+        scrollToStrapline();
+    });
 });
