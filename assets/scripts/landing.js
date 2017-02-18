14e8a7bf21e810f0b030acd8d438f5b3ee8e253f
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-02-18 15:06:48 +0000

    Added fade in effects

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,2 +1,17 @@
 //document.ready / window.onload / self-calling function
+var scrollHandler = function(){
+    // Detect if the strap line container is visible and if so animate it in
+    if ($('.strap-line').visible()) {
+        $('.strap-line-upper').addClass('appear');
+        $('.strap-line-lower').addClass('appear');
+        $('.info-image').addClass('appear');
+        $(document).off('scroll', scollHandler);        
+    }
+};
 
+
+$(document).ready(function(){
+    $(this).on('scroll',scrollHandler);
+//    $('.strap-line-upper').hide();
+//    $('.strap-line-lower').hide();
+});
