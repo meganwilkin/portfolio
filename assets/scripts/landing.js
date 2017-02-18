786c64c24d71eb63dda6eb089ee06cfc65d98373
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-02-18 18:23:45 +0000

    Intial version completed

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,9 +1,9 @@
 //document.ready / window.onload / self-calling function
 var scrollHandler = function(){
     // Detect if the strap line container is visible and if so animate it in
-    if ($('.strap-line').visible()) {
-        $('.strap-line-upper').addClass('appear');
-        $('.strap-line-lower').addClass('appear');
+    if ($('#strap-line').visible()) {
+        $('#strap-line-upper').addClass('appear');
+        $('#strap-line-lower').addClass('appear');
         $('.info-image').addClass('appear');
         $(document).off('scroll', this);        
     }
@@ -11,14 +11,14 @@ var scrollHandler = function(){
 
 var scrollToStrapline = function() {
     $('html, body').animate({
-        scrollTop: $(".strap-line-upper").offset().top - 70
+        scrollTop: $("#strap-line-upper").offset().top - 70
     }, 2000);
 };
 
 $(document).ready(function(){
     $(this).on('scroll',scrollHandler);
     
-    if ($('.strap-line').visible()) {
+    if ($('#strap-line').visible()) {
         scrollHandler();
     }
     
